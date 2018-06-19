b1726eb47cee707d191664b597cce607be4ed661
Gocha <sys.gocha@gmail.com>
2018-06-18 20:39:45 -0400

    Changes in SexyGochaController

--- WebApplication1/Controllers/SexyGochaController.cs
+++ WebApplication1/Controllers/SexyGochaController.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using System.Web;
 using System.Web.Mvc;
 
@@ -11,7 +12,10 @@ namespace WebApplication1.Controllers
         // GET: SexyGocha
         public ActionResult Index()
         {
+            StringBuilder str = new StringBuilder();
             return View();
         }
+
+
     }
 }
